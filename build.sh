6f3f80a1659f0c5403baaf122dd1afd3a2ac9f5e
Fureev Eugene <fureev.cyprus@gmail.com>
2023-04-17 12:32:47 +0300

    deps

--- build.sh
+++ build.sh
@@ -1,22 +1,34 @@
 #!/usr/bin/env bash
 
+args=""
+
+if [[ $(uname -p) == 'arm' ]]; then
+  args="-a arm64"
+  echo "Building for ARM"
+fi
+
 ./node_modules/.bin/nativefier https://music.yandex.ru \
   --single-instance \
+  --disable-old-build-warning-yesiknowitisinsecure \
+  --verbose \
   --name Yandex.Music \
   --icon ./og-image.png \
-  --electron-version 15.0.0 \
-  --inject ./renderer.js \
-  --inject ./style.css \
   --width 1280px \
   --height 800px \
+  --electron-version 22.3.6 \
+  --inject ./renderer.js \
+  --inject ./style.css \
   --fast-quit \
   --title-bar-style 'hiddenInset' \
   --darwin-dark-mode-support \
-  --internal-urls "(?:music|passport)\.yandex\.ru/.*"
+  --internal-urls "(?:music|passport)\.yandex\.ru/.*" \
+  --disable-dev-tools \
+  --disable-context-menu \
+  -v 0.2.0 \
+  $args
 
-#  --version 0.1.1
-  #  --portable \
+#  --portable \
 
 #  --disable-context-menu \
 #  --disable-dev-tools
-  # --global-shortcuts ./shortcuts.json \
+# --global-shortcuts ./shortcuts.json \
